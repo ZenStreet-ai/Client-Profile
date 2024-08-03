a4c11a7b4e2c55114ced1fe9cc313fbdd99db4e6
Anshumohan Acharya <anshumohanacharya19@gmail.com>
2024-08-03 18:39:03 +0530

    Custom Button Created

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, ButtonProps } from "./ui/button";
+import clsx from "clsx";
+
+interface CustomButtonProps extends ButtonProps {
+  variant?: "primary" | "secondary";
+  size?: "primary" | "secondary";
+  active?: boolean;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  variant,
+  size,
+  active,
+  ...props
+}) => {
+  const baseStyles = "transition-colors duration-200 ease-in-out ";
+  const activeStyles = active ? "" : "bg-gray-300 text-gray-500 cursor-not-allowed";
+
+  return (
+    <Button className={clsx(baseStyles, activeStyles)} variant={variant} size={size} {...props} disabled={!active}>
+      {props.children}
+    </Button>
+  );
+};
+
+export default CustomButton;
