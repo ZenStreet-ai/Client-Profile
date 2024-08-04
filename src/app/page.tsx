8df98f72dc11435efd44726d8374107f38fe1c92
Anshumohan Acharya <anshumohanacharya19@gmail.com>
2024-08-04 23:53:38 +0530

    Profile Screen Skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const pathname = usePathname();
+  useEffect(() => {
+    if (pathname === "/") {
+      router.replace("/profile");
+    }
+  }, [router, pathname]);
   return <div></div>;
 }
